ae9d621bfffca29b8f79df7042ab6a87e107cd15
Justin <j.hebenstreit25@gmail.com>
2024-08-05 23:07:09 -0500

    hopefully resolved  some merge conflicts

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -287,13 +287,14 @@ class Cli {
       ])
       .then((answers) => {
 
-        if (vehicle.vin === answers.vehicleToTow.vin) {
-          console.log(`This vehicle cannot tow itself.`)
+        if (answers.vehicleToTow === truck) {
+          console.log(`${Truck}.} cannot tow itself.`)
         } else {
-          vehicle.tow(answers.vehicleToTow)
-        }
+          truck.tow(answers.vehicleToTow)
+          // start the cli to return to the initial prompt if the user wants to select or create another vehicle
+          this.performActions();
       }
-      );
+  });
   }
 
   // method to perform actions on a vehicle
@@ -397,7 +398,7 @@ class Cli {
             }
           }
         }
-        // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
+          // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
 
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
         else if (answers.action === 'Select or create another vehicle') {
